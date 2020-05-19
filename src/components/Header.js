4208a71a97eb56185d798a0a82c301f3acf25230
Keehada <frankgquijada@hotmail.com>
2020-05-19 12:29:54 -0500

    Header redone

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,26 @@ import { Link } from 'react-router-dom';
 
 function Header() {
 	return (
-		<nav className="navbar justify-content-center fixed-top">
-			<li className="nav-item nav-link">
-				<Link to="/" className="nav-text">
-					Top Games
-				</Link>
-			</li>
-			<li className="nav-item nav-link">
-				<Link to="/top-streams" className="nav-text">
-					Top Streams
-				</Link>
-			</li>
+		<nav className="navbar navbar-expand-md fixed-top">
+			<span classname="navbar-brand">
+				<h6 className="nav-text" id="title">
+					Twitch Leaderboard
+				</h6>
+			</span>
+			<div class="collapse navbar-collapse">
+				<ul className="navbar-nav ml-auto">
+					<li className="nav-item nav-link">
+						<Link to="/" className="nav-text">
+							Top Games
+						</Link>
+					</li>
+					<li className="nav-item nav-link">
+						<Link to="/top-streams" className="nav-text">
+							Top Streams
+						</Link>
+					</li>
+				</ul>
+			</div>
 		</nav>
 	);
 }
